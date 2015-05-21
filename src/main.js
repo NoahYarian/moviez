cf5d1f03e544276eb9ddf1546465bcc32a0e52d8
Noah Yarian <noah.yarian@gmail.com>
2015-05-21 10:03:07 -0500

    added login fields and code from scott's codepen

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 var OMDB_URL = "http://www.omdbapi.com/?";
-var TMDB_URL = "https://api.themoviedb.org/3/search/movie?api_key=eccd2504f86260d8315b3dbf909e2c1e"
-var TMDB_IMG_URL = "http://image.tmdb.org/t/p/w500/"
 var FIRE_URL = "https://search4moviez.firebaseio.com/movies.json";
 var $searchInput = $(".search input");
 var $searchResults = $(".searchResults");
@@ -113,17 +111,123 @@ $watchlist.on("click", "button", function() {
 
 });
 
-//function addHeaders() {
-  //var html = "<div class='movie header'>";
-  //html += "<div>Poster</div>";
-  //html += "<div>Title</div>";
-  //html += "<div>Year</div>";
-  //html += "<div>Tomatometer</div>";
-  //html += "<div>Certified Fresh</div>";
-  //html += "<div>IMDb Rating</div>";
-  //html += "<div>IMDb Votes</div>";
-  //html += "<div>Metascore</div>";
-  //html += "<div>Remove</div>";
-  //html += "</div>";
-  //$watchlist.append(html);
-//}
+/////////////////////
+/// Login madness ///
+/////////////////////
+
+var FIREBASE_URL = 'https://search4moviez.firebaseio.com';
+var fb = new Firebase(FIREBASE_URL);
+
+$('.onTempPassword form').submit(function () {
+  var email = fb.getAuth().password.email;
+  var oldPw = $('.onTempPassword input:nth-child(1)').val();
+  var newPw = $('.onTempPassword input:nth-child(2)').val();
+
+  fb.changePassword({
+    email: email,
+    oldPassword: oldPw,
+    newPassword: newPw
+  }, function(err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      fb.unauth();
+    }
+  });
+
+  event.preventDefault();
+})
+
+$('.doResetPassword').click(function () {
+  var email = $('.onLoggedOut input[type="email"]').val();
+
+  fb.resetPassword({
+    email: email
+  }, function (err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      alert('Check your email!');
+    }
+  });
+});
+
+$('.doLogout').click(function () {
+  fb.unauth();
+})
+
+$('.doRegister').click(function () {
+  var email = $('.onLoggedOut input[type="email"]').val();
+  var password = $('.onLoggedOut input[type="password"]').val();
+
+  fb.createUser({
+    email: email,
+    password: password
+  }, function (err, userData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      doLogin(email, password);
+    }
+  });
+
+  event.preventDefault();
+});
+
+$('.onLoggedOut form').submit(function () {
+  var email = $('.onLoggedOut input[type="email"]').val();
+  var password = $('.onLoggedOut input[type="password"]').val();
+
+  doLogin(email, password);
+  event.preventDefault();
+});
+
+function clearLoginForm () {
+  $('input[type="email"]').val('');
+  $('input[type="password"]').val('');
+}
+
+function saveAuthData (authData) {
+  $.ajax({
+    method: 'PUT',
+    url: `${FIREBASE_URL}/users/${authData.uid}/profile.json`,
+    data: JSON.stringify(authData)
+  });
+}
+
+function doLogin (email, password, cb) {
+  fb.authWithPassword({
+    email: email,
+    password: password
+  }, function (err, authData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      saveAuthData(authData);
+      typeof cb === 'function' && cb(authData);
+    }
+  });
+}
+
+fb.onAuth(function (authData) {
+  var onLoggedOut = $('.onLoggedOut');
+  var onLoggedIn = $('.onLoggedIn');
+  var onTempPassword = $('.onTempPassword');
+
+  if (authData && authData.password.isTemporaryPassword) {
+    onTempPassword.removeClass('hidden');
+    onLoggedIn.addClass('hidden');
+    onLoggedOut.addClass('hidden');
+  } else if (authData) {
+    onLoggedIn.removeClass('hidden');
+    onLoggedOut.addClass('hidden');
+    onTempPassword.addClass('hidden');
+    $('.onLoggedIn h1').text(`Hello ${authData.password.email}`);
+  } else {
+    onLoggedOut.removeClass('hidden');
+    onLoggedIn.addClass('hidden');
+    onTempPassword.addClass('hidden');
+  }
+
+  clearLoginForm();
+});
